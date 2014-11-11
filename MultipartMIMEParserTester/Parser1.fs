a52e1fc9c4ed6fc1db1355df0459faf1bccb6b92
Jeff Maner <jeffmaner@yahoo.com>
2014-11-11 07:27:54 -0600

    Fresh start with Parser1, relying on streams rather than strings. Also added XUnit and tests. VS doesn't recognize the tests, but they explode when they fail.

---
+++ MultipartMIMEParserTester/Parser1.fs
@@ -0,0 +1,38 @@
+﻿namespace MultipartMIMEParser
+
+open FParsec
+open System.IO
+
+type private Post1 = { contentType : string; boundary : string; subtype : string; content : string }
+
+module Parser1 =
+  ()
+
+type MParser1(s:Stream) =
+  let ($) f x = f x
+  let ascii = System.Text.Encoding.ASCII
+  let str cs = System.String.Concat (cs:char list)
+  let q = "\""
+  let qP = pstring q
+  let pSemicolon = pstring ";"
+  let manyNoDoubleQuote = many $ noneOf q
+  let enquoted = between qP qP manyNoDoubleQuote |>> str
+  let skip = skipStringCI
+  let pContentType = skip "content-type: "
+                     >>. manyTill anyChar (attempt $ preturn () .>> pSemicolon)
+                     |>> str
+  let pBoundary = skip " boundary=" >>. enquoted
+  let pSubtype = opt $ pSemicolon >>. skip " type=" >>. enquoted
+  let pContent = many anyChar |>> str // TODO: The content parser needs to be recursive on the stream.
+  let pStream = pipe4 pContentType pBoundary pSubtype pContent
+                      $ fun c b t s -> { contentType=c; boundary=b; subtype=t; content=s }
+  let result s = match runParserOnStream pStream () "" s ascii with
+                  | Success (r,_,_) -> r
+                  | Failure (e,_,_) -> failwith (sprintf "%A" e)
+  let r = result s
+  member p.ContentType = r.contentType
+  member p.Boundary = r.boundary
+  member p.ContentSubtype = r.subtype
+  member p.Content = r.content
+
+// vim:ft=fs
--- MultipartMIMEParserTester/Program.fs
+++ MultipartMIMEParserTester/Program.fs
@@ -1,5 +1,22 @@
 ﻿open MultipartMIMEParser
 open System.IO
+open Xunit
+
+let path = @"C:\Users\jeff.maner\Documents\Projects\RosettaNet\"
+let file = "sampleRNIFReceiptAck.txt"
+let text = File.ReadAllText(path+file)
+let s = new FileStream(path+file, FileMode.Open)
+
+let parser = new MParser1(s)
+
+[<Fact>]
+let canReadContentType = Assert.Equal<string> ("Multipart/related", parser.ContentType)
+
+[<Fact>]
+let canReadBoundary = Assert.Equal<string> ("RN-Http-Body-Boundary", parser.Boundary)
+
+[<Fact>]
+let canReadSubtype = Assert.Equal<string> ("multipart/related", parser.ContentSubtype)
 
 [<EntryPoint>]
 let main args =
@@ -10,8 +27,9 @@ let main args =
     //let file = "samplePart1.txt"
     let text = File.ReadAllText(path+file)
     use s = new FileStream(path+file, FileMode.Open)
-    let parser = new MParser(s)
+    let parser = new MParser1(s)
 
+    (*
     printfn "when I ask for the entire stream as a string, I get it."
     printfn "%A" (parser.ToString() = text)
 
@@ -22,12 +40,20 @@ let main args =
     printfn "%A" (parser.Post.postHeaders)
     printfn "%A" (parser.Post.postContent)
 
+    *)
+
     printfn "when I ask for the boundary, I get it."
     printfn "%A" (parser.Boundary = "RN-Http-Body-Boundary")
 
+    (*
     printfn "when I ask for the count of parts, I get one."
     printfn "%A" parser.Parts
     printfn "%A" (parser.Parts.Length = 1)
+
+    let mainDoc = parser.Parts.Head
+    for p in mainDoc.Parts do
+        printfn "%s" p.Boundary
+    *)
 
     System.Console.ReadKey() |> ignore
     0
